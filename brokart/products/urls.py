7023e6d689a5dc0d60fb22d4ac104be384e0ffa0
anatalphonset <anatalphonset@gmail.com>
2025-02-12 00:37:11 +0530

    product layout added

--- brokart/products/urls.py
+++ brokart/products/urls.py
@@ -3,5 +3,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from .import views
 urlpatterns = [
-    path('',views.index),   
+    path('',views.index,name='home'),   
+    path('product_list',views.list_products,name='list_products')
 ]
